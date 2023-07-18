75a87fa6882056078739a93dd307cfab877d6857
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2023-07-18 18:51:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let box = document.querySelector(".box");
+let bott = document.querySelector(".bott");
+let coniner = document.querySelector(".coniner");
+
+box.addEventListener("click", () => {
+  bott.classList.toggle("show");
+});
